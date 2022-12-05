38ee660cac07ac5431ad615457323ebcf3bc95a4
María Alejandra Ramírez <ramirez@evolbio.mpg.de>
2022-12-05 11:48:18 +0100

    Add functions to graph Average payoff contour plot

--- Intros_Dyn/Stationary_Distribution/Graphs.py
+++ Intros_Dyn/Stationary_Distribution/Graphs.py
@@ -128,3 +128,65 @@ plt.contourf(B,R,np.transpose(Big_Val),levels =n,cmap="inferno")
 plt.colorbar(ticks=[2,10,20,30,40,50,60,70,80,90], label = "Average claim")
 plt.savefig("Graphs/Contour_Gen_Z.png")
 plt.clf()
+
+#---------AVERAGE PAYOFF-------
+"""
+    payoff(x,y,R) -> Float64
+Defines the payoff scheme of the Travelers Dilemma
+Note: I am x, other player is y, for there to be incentive in playing low claims
+ If switched, payoff scheme changes
+Param: x My claim
+Param: y Other player claim
+Param: R reward/punishment in the payoff scheme
+Return: My payoff
+"""
+def payoff(x,y,r):
+    if x > y:
+        return min(x, y) - r
+    elif x < y:
+        return min(x, y) + r
+    else:
+        return min(x,y)
+
+
+"""
+    payoff_matrix(R) -> Matrix{Float64}
+Payoff matrix of Travelers Dilemma
+Returns TD payoff matrix for a given reward value
+Param: n Number of claims
+Param: R reward/punishment in the payoff scheme
+Return: Payoff Matrix
+"""
+def payoff_matrix(r):
+    n=99
+    A = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            A[i,j] = payoff(i+2,j+2,r)
+            #Sum 1 in i and j because strategy space is [2,100]
+    return A
+
+B = [0.01,0.02,0.04,0.05,0.06,0.08,0.1,0.2,0.4,0.5,0.6,0.8,1.0]
+R = [2,5,10,15,20,25,30,35,40]
+
+values_B = np.empty((np.size(B),n))
+values_R = np.empty((np.size(R),n))
+
+Big_Val = np.empty((np.size(B),np.size(R)))
+
+#Obtain information from files
+for i in range(np.size(B)):
+    for j in range(np.size(R)):
+        payM = payoff_matrix(j)
+        info = np.genfromtxt("Data/Results_"+str(B[i])+"_"+str(R[j])+".txt")
+        ans1 = np.dot(payM,info)
+        ans2 = np.dot(ans1,info)
+        Big_Val[i,j] = ans2
+
+plt.xscale("log")
+plt.xlabel("Selection intensity coefficient ("+r"$\beta$"+")")
+plt.ylabel("Reward parameter (R)")
+plt.contourf(B,R,np.transpose(Big_Val),levels =n,cmap="inferno")
+plt.colorbar(ticks=[2,10,20,30,40,50,60,70,80,90], label = "Average payoff")
+plt.savefig("Graphs/Contour_Avgpayoff.png")
+plt.clf()
